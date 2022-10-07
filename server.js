d73d73f7417046590a1f4a0ef3da4a621791f796
sanjay <84283520+lsanjayl@users.noreply.github.com>
2022-10-07 07:16:26 +0530

    final

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const app = express();
+app.use(express.static('public'));
+app.use(express.json({limit:'1mb'}));
 const nodemailer=require("nodemailer")
 const cors = require('cors') 
 app.use(cors({
@@ -9,29 +11,29 @@ const PORT =process.env.PORT||5000;
 app.post('/', (req, res) => {
     console.log(req.body);
     // res.send("helloa");
-    // const transporter =nodemailer.createTransport({
-    //     service:"gmail",
-    //     auth:{
-    //         user:'contact@creatorslab.in',
-    //         pass:'ottfyrvkhldwcfwu'
-    //     }
-    // })
-    // const mailOptions = {
-    //     from:req.body.email,
-    //     to:'jabez@creatorslab.in',
-    //     subject:`Message from ${req.body.email}:${req.body.name}`,
-    //     html:`<p>Name:${req.body.name}</p><p>Phone:${req.body.phone}</p><p>Department:${req.body.department}</p><p>Collegename:${req.body.collegename}</p><p>Currentyear:${req.body.currentyear}</p><p>Message:${req.body.message}</p>`,
-    // }
-    // transporter.sendMail(mailOptions,(error,info)=>{
-    //     if(error){
-    //         console.log(error);
-    //         res.send('error');
-    //     }
-    //     else{
-    //         console.log('Email sent');
-    //         res.send('success');
-    //     }
-    // })
+    const transporter =nodemailer.createTransport({
+        service:"gmail",
+        auth:{
+            user:'contact@creatorslab.in',
+            pass:'ottfyrvkhldwcfwu'
+        }
+    })
+    const mailOptions = {
+        from:req.body.email,
+        to:'jabez@creatorslab.in',
+        subject:`Message from ${req.body.email}:${req.body.name}`,
+        html:`<p>Name:${req.body.name}</p><p>Phone:${req.body.phone}</p><p>Department:${req.body.department}</p><p>Collegename:${req.body.collegename}</p><p>Currentyear:${req.body.currentyear}</p><p>Message:${req.body.message}</p>`,
+    }
+    transporter.sendMail(mailOptions,(error,info)=>{
+        if(error){
+            console.log(error);
+            res.send('error');
+        }
+        else{
+            console.log('Email sent');
+            res.send('success');
+        }
+    })
 });
 app.listen(PORT,()=>{
     console.log(`Server running on port ${PORT}`);
